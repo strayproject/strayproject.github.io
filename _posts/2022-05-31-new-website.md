aca9b5fa5351464c6f8b4c82b9d89b0f8471e685
brandon pow <pow.brandon@gmail.com>
2022-05-31 23:56:50 +0800

    Initial commit

---
+++ _posts/2022-05-31-new-website.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: New Website!
+---
+Welcome to the new website for the Stray Project! The team will use this website to update you viewers on what projects we might be cooking up. Stay tuned!
+
+
+- bp
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+]
+}
