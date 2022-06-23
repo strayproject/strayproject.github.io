c468e03b5d6288a6bed845fa8a0ed19d7da84790
brandon pow <pow.brandon@gmail.com>
2022-06-24 03:55:21 +0800

    added music player (wip)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './_drafts/**/*.html',
-    './_includes/**/*.html',
+    './_includes/**/*.{html,js}',
     './_layouts/**/*.html',
     './_posts/*.md',
     './*.{md,html}',
