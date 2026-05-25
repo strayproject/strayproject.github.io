53dd94bf777c90eed8785c369fe2014db645a1d0
brandon pow <pow.brandon@gmail.com>
2026-05-25 23:29:46 +0800

    audio revamp

---
+++ src/data/strandedMusic.ts
@@ -0,0 +1,67 @@
+export const strandedMusicTracks = [
+  {
+    title: "Stranded (Main Theme)",
+    artist: "Rex Revon",
+    duration: "1:33",
+    src: "/assets/audio/stranded.ogg",
+    cover: "/assets/img/player/AlbumCover.webp",
+  },
+  {
+    title: "Intense",
+    artist: "Rex Revon",
+    duration: "1:23",
+    src: "/assets/audio/intense.ogg",
+    cover: "/assets/img/player/AlbumCover.webp",
+  },
+  {
+    title: "Sun (Day)",
+    artist: "Rex Revon",
+    duration: "1:14",
+    src: "/assets/audio/sunday.ogg",
+    cover: "/assets/img/player/AlbumCover.webp",
+  },
+  {
+    title: "Hopeless Harmony",
+    artist: "Rex Revon",
+    duration: "1:36",
+    src: "/assets/audio/hopeless.ogg",
+    cover: "/assets/img/player/AlbumCover.webp",
+  },
+  {
+    title: "Chaotic Dumbass",
+    artist: "Rex Revon",
+    duration: "0:22",
+    src: "/assets/audio/chaotic.ogg",
+    cover: "/assets/img/player/AlbumCover.webp",
+  },
+  {
+    title: "HeartFelt Midnight",
+    artist: "Zammu",
+    duration: "1:59",
+    src: "/assets/audio/heartfelt.ogg",
+    cover: "/assets/img/player/AlbumCover2.webp",
+  },
+  {
+    title: "Sincerely",
+    artist: "Zammu",
+    duration: "2:20",
+    src: "/assets/audio/sincerely.ogg",
+    cover: "/assets/img/player/AlbumCover2.webp",
+  },
+  {
+    title: "Downpour",
+    artist: "Zammu",
+    duration: "1:15",
+    src: "/assets/audio/downpour.mp3",
+    cover: "/assets/img/player/AlbumCover2.webp",
+  },
+  {
+    title: "Pinky Promise",
+    artist: "Zammu",
+    duration: "2:46",
+    src: "/assets/audio/pinkypromise.mp3",
+    cover: "/assets/img/player/AlbumCover2.webp",
+  },
+];
+
+export type AudioTrack = (typeof strandedMusicTracks)[number];
