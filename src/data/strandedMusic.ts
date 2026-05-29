21d6506132655c00b58d01a7f1ba5d3e615bd4f6
brandon pow <pow.brandon@gmail.com>
2026-05-29 19:08:30 +0800

    astro image optimization, darkening character selector

--- src/data/strandedMusic.ts
+++ src/data/strandedMusic.ts
@@ -1,66 +1,69 @@
+import albumCover from "../assets/stranded/player/AlbumCover.webp";
+import albumCover2 from "../assets/stranded/player/AlbumCover2.webp";
+
 export const strandedMusicTracks = [
   {
     title: "Stranded (Main Theme)",
     artist: "Rex Revon",
     duration: "1:33",
     src: "/assets/audio/stranded.ogg",
-    cover: "/assets/img/player/AlbumCover.webp",
+    cover: albumCover,
   },
   {
     title: "Intense",
     artist: "Rex Revon",
     duration: "1:23",
     src: "/assets/audio/intense.ogg",
-    cover: "/assets/img/player/AlbumCover.webp",
+    cover: albumCover,
   },
   {
     title: "Sun (Day)",
     artist: "Rex Revon",
     duration: "1:14",
     src: "/assets/audio/sunday.ogg",
-    cover: "/assets/img/player/AlbumCover.webp",
+    cover: albumCover,
   },
   {
     title: "Hopeless Harmony",
     artist: "Rex Revon",
     duration: "1:36",
     src: "/assets/audio/hopeless.ogg",
-    cover: "/assets/img/player/AlbumCover.webp",
+    cover: albumCover,
   },
   {
     title: "Chaotic Dumbass",
     artist: "Rex Revon",
     duration: "0:22",
     src: "/assets/audio/chaotic.ogg",
-    cover: "/assets/img/player/AlbumCover.webp",
+    cover: albumCover,
   },
   {
     title: "HeartFelt Midnight",
     artist: "Zammu",
     duration: "1:59",
     src: "/assets/audio/heartfelt.ogg",
-    cover: "/assets/img/player/AlbumCover2.webp",
+    cover: albumCover2,
   },
   {
     title: "Sincerely",
     artist: "Zammu",
     duration: "2:20",
     src: "/assets/audio/sincerely.ogg",
-    cover: "/assets/img/player/AlbumCover2.webp",
+    cover: albumCover2,
   },
   {
     title: "Downpour",
     artist: "Zammu",
     duration: "1:15",
     src: "/assets/audio/downpour.mp3",
-    cover: "/assets/img/player/AlbumCover2.webp",
+    cover: albumCover2,
   },
   {
     title: "Pinky Promise",
     artist: "Zammu",
     duration: "2:46",
     src: "/assets/audio/pinkypromise.mp3",
-    cover: "/assets/img/player/AlbumCover2.webp",
+    cover: albumCover2,
   },
 ];
 
